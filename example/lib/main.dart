beac6057aa050990497000c0d61305fcaa20be0f
zyjAndroid <binbin1314>
2019-12-05 15:10:01 +0800

    EEEEEE

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -51,6 +51,7 @@ class MyApp extends StatelessWidget {
 class PermissionWidget extends StatefulWidget {
   const PermissionWidget(this._permissionGroup);
   //final PermissionGroup _dd;
+  //eeeeee
   final PermissionGroup _permissionGroup;
 
   @override
